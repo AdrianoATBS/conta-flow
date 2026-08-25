58d638413888457424f2dd11e500b572247c63e3
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-08-25 09:51:14 -0300

    feat(login): permite personalizar estilo do subtítulo de autenticação

--- src/features/autenticacao/login/LoginPage.tsx
+++ src/features/autenticacao/login/LoginPage.tsx
@@ -7,7 +7,9 @@ export default function LoginPage(){
         <div className=" w-full min-h-screen flex flex-col justify-between items-center gap-4" >
             <div className="w-full flex flex-col items-center gap-4 p-4 py-12">
 
-                <HeaderAutenticacao Titulo="Acesse sua conta" Subtitulo="Bem-vindo de volta ao SaaS Manager." />
+                <HeaderAutenticacao Titulo="Acesse sua conta" Subtitulo="Bem-vindo de volta ao SaaS Manager."
+                EstiloSubtitulo="text-sm md:text-base"
+                />
                 <FormularioLogin />
             </div>
             <FooterAutenticacao/>
