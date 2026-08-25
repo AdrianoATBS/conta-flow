737f331940c6cadab3c8e96c5b10a575e0c2ed53
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-08-25 10:50:57 -0300

    feat(autenticacao): adiciona suporte a estilo personalizado no subtítulo

--- src/features/autenticacao/components/HeaderAutenticacao.tsx
+++ src/features/autenticacao/components/HeaderAutenticacao.tsx
@@ -1,13 +1,14 @@
 interface HeaderAutenticacaoProps  {
     Titulo: string,
     Subtitulo: string
+    EstiloSubtitulo?: string
 }
 
-export default function HeaderAutenticacao({ Titulo, Subtitulo }: HeaderAutenticacaoProps){
+export default function HeaderAutenticacao({ Titulo, Subtitulo, EstiloSubtitulo  }: HeaderAutenticacaoProps){
     return(
         <header className="text-center">
                <h1 className="text-[#1C1B1B] text-2xl font-bold">{Titulo}</h1>       
-               <p className="text-[#5E5E5E] text-sm">{Subtitulo}</p>
+               <p className={`text-[#5E5E5E] ${EstiloSubtitulo || "text-sm"}`}>{Subtitulo}</p>
         </header>
     )
 }
