35cc1c5fee8f54ad7317f5f049c6b86688c82046
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-08-28 11:07:08 -0300

    feat(cadastro): adiciona lógica para registrar usuario

--- src/features/autenticacao/components/FormularioCadastro.tsx
+++ src/features/autenticacao/components/FormularioCadastro.tsx
@@ -1,35 +1,73 @@
+"use client";
 import BotaoGenerico from "@/shared/components/ui/BotaoGenerico";
 import InputGenerico from "@/shared/components/ui/InputGenerico";
 import Link from "next/link";
+import { useState } from "react";
+import { SubmitEvent } from "react";
+import { registroService } from "../services/registro.services";
 export default function FormularioCadastro() {
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(!nome || !email || !senha){
+            alert("Por favor, preencha todos os campos.");
+            return; 
+        }
+        
+        try {
+            const response = await registroService({ nome, email, senha });
+            console.log(response);
+        }catch(error){
+            console.error("Erro ao registrar:", error);
+        }
+
+        setNome("");
+        setEmail("");
+        setSenha("");
+    }
+    
     return(
         <section className="w-full max-w-md flex flex-col items-center gap-4 p-4 ">
 
-            <form className="w-full border-borda border p-6 
+            <form onSubmit={handleSubmit} className="w-full border-borda border p-6 
             sm:p-8 rounded-2xl flex flex-col gap-4 justify-center " >
                 <div className="flex flex-col gap-1.5 ">
                     <label className="text-xs text-[#5E5E5E] md:text-sm">Nome</label>
-                    <InputGenerico placeholder="Seu nome completo" type="text"  tamanho="sm" cores="secundaria"
-                    className="rounded-lg border-borda outline-none"/>
+                    <InputGenerico placeholder="Seu nome completo" type="text" 
+                     tamanho="sm" cores="secundaria"
+                    className="rounded-lg border-borda outline-none"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}/>
                 </div>
 
                 <div className="flex flex-col gap-1.5 ">
                     <label className="text-xs text-[#5E5E5E] md:text-sm">Email</label>
                     <InputGenerico placeholder="exemplo@dominio.com" type="email" tamanho="sm" cores="secundaria"
-                    className="rounded-lg border-borda outline-none"/>
+                    className="rounded-lg border-borda outline-none"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}>
+                    </InputGenerico>
                 </div>
 
                 <div className="flex flex-col gap-1.5 ">
                     <label className="text-xs text-[#5E5E5E] md:text-sm">Senha</label>
                     <InputGenerico placeholder="••••••••" type="password"  tamanho="sm" cores="secundaria"
-                    className="rounded-lg border-borda outline-none"/>
+                    className="rounded-lg border-borda outline-none"
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}/>
                 </div>
 
-                <div className="flex flex-col gap-1.5 " >
+                {/* <div className="flex flex-col gap-1.5 " >
                     <label className="text-xs text-[#5E5E5E] md:text-sm">Confirmar Senha</label>
                     <InputGenerico placeholder="••••••••" type="password"  tamanho="sm" cores="secundaria"
-                    className="rounded-lg border-borda outline-none"/>
-                </div>
+                    className="rounded-lg border-borda outline-none"
+                    value={senha}
+                    onChange={(e) => setConfirmarSenha(e.target.value)} />
+                </div> */}
 
                 <div className="flex flex-col gap-1.5 ">
                 <BotaoGenerico texto="Criar Conta"  className="hover:bg-black"/>
