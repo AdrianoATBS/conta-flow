2c9f2e92ac26489ec50dd91deff5d6fe61f42bdc
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-08-28 11:05:40 -0300

    feat(lib): adiciona cliente HTTP genérico com o fetcher

---
+++ src/lib/fetcher.ts
@@ -0,0 +1,11 @@
+import { BASE_URL } from "./api";
+
+export default async function fetcher<T>(endpoint:string, options?: 
+    RequestInit): Promise<T>{
+        
+        const response = await fetch(`${BASE_URL}${endpoint}`, options);
+        if(!response.ok){
+            throw new Error(`Error na requisição: ${response.statusText}` );
+        }
+        return await response.json();
+}
