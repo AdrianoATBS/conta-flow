e2519c0ad405349476a9557bf44d37a2dd65f33d
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-09-01 10:09:30 -0300

    feat(painel): adiciona header do painel utilizando layout base

---
+++ src/features/painel/painelPage/PainelPage.tsx
@@ -0,0 +1,9 @@
+import HeaderPainel from "./components/HeaderPainel";
+
+export default function PainelPage(){
+    return(
+        <div className="w-full h-screen">
+            <HeaderPainel />
+        </div>
+    )
+}
---
+++ src/features/painel/painelPage/components/HeaderPainel.tsx
@@ -0,0 +1,18 @@
+import {Header, Logo, Icons, BotaoLogout} from "@/shared/components/layout/HeaderBase";
+
+export default function HeaderPainel() {
+    return(
+        <header>
+            <Header 
+                esquerda={<Logo EhHome={true} />}
+                centro={null}
+                direita={
+                <div className="flex items-center gap-1">
+                    <BotaoLogout/> 
+                    <Icons />
+                </div>
+                }
+            />
+        </header>
+    )
+}
