e64ec50d3e933cb6de0f5d6da8314fee03d519a4
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-09-09 10:53:56 -0300

    feat(configuracoes): adiciona titulo generico na página de configurações

--- src/features/painel/configuracoesPage/ConfiguracaoPage.tsx
+++ src/features/painel/configuracoesPage/ConfiguracaoPage.tsx
@@ -1,3 +1,4 @@
+import TituloGenerico from "../components/TituloGenerico";
 import HeaderConfiguracoes from "./components/HeaderConfiguracoes";
 import SecaoConfiguracoes from "./components/NavConfiguracoes";
 export default function ConfiguracaoPage() {
@@ -7,6 +8,8 @@ export default function ConfiguracaoPage() {
             <div className="w-full max-w-2xl mx-auto pt-6 px-4
             flex flex-col gap-2 h-full">
                 <SecaoConfiguracoes />
+                <TituloGenerico titulo="Configurações do Perfil" 
+                texto="Gerencie suas informações pessoais e preferências de conta."/>
             </div>
         </div>
     )
