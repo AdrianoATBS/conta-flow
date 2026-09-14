4c2cafaed8a58053f351b1540ae4a91d941ee6a5
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-09-14 18:49:03 -0300

    style(titulo): adiciona margem inferior

--- src/features/painel/components/TituloGenerico.tsx
+++ src/features/painel/components/TituloGenerico.tsx
@@ -4,7 +4,7 @@ interface TituloGenericoProps {
 }
 export default function TituloGenerico({ titulo, texto }: TituloGenericoProps){
     return(
-        <div className="w-full flex flex-col gap-1">
+        <div className="w-full flex flex-col gap-1 mb-5">
             <h1 className="text-[32px] font-semibold">{titulo}</h1>
             <p className="text-sm text-[#5E5E5E]">{texto}</p>
         </div>
