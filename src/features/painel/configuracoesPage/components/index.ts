a113790f30787c092018570dacb686c6e25ddf22
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-09-14 20:34:28 -0300

    refactor(configuracao): centraliza exports dos componentes em index.ts

---
+++ src/features/painel/configuracoesPage/components/index.ts
@@ -0,0 +1,5 @@
+export {default as EditarPerfil} from "./EditarPerfil";
+export {default as HeaderConfiguracoes} from "./HeaderConfiguracoes";
+export {default as SecaoConfiguracoes} from "./NavConfiguracoes";
+export {default as StatusConta} from "./StatusConta";
+export {default as ZonaPerigo} from "./ZonaPerigo";
